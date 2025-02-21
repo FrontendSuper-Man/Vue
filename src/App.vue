6ac0546d7543e367820a928890b92c290ab90de4
Sunnat Khudoykulov <khudoykulov07@bk.ru>
2025-02-21 21:11:28 +0500

    Сделал 2 дз

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,17 @@
 <template>
-  <div id="app">Hello Vue.js</div>
+  <div class="wrapper">
+    <HeroView />
+    <FooterComponent />
+  </div>
 </template>
 
 <script>
-export default {};
+import HeroView from "./views/HeroView.vue";
+import FooterComponent from "./views/FooterComponent.vue";
+
+export default {
+  components: { HeroView, FooterComponent },
+};
 </script>
 
 <style></style>
